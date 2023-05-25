83cf999b7584c17251f8dd62922e90ce9f8a0615
remiie <virusloki@ya.ru>
2023-05-25 19:44:43 +0300

    add entity

--- WeatherApp/Modules/Weather/WeatherEntity.swift
+++ WeatherApp/Modules/Weather/WeatherEntity.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+
+struct WeatherEntity: Decodable{
+    var temp: Double
+}
+
+struct WeatherGroupResponse: Decodable {
+    let list: [WeatherResponse]
+}
+
+struct WeatherResponse: Decodable {
+    let id: Int
+    let name: String
+    let main: WeatherEntity
+}
+
+//ограничение на 10 городов, удалить первый добавить последний
